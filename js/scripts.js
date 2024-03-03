b36209b895691aa102dcafa2f926f8954436aa09
juan Gonzalez <juan.gonzalez@idaesoluciones.com>
2024-03-03 17:58:15 -0500

    original

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+/*!
+    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2023 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    // 
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+    // Abrir el modal al hacer clic en el botón
+    document.getElementById("openModalBtn").addEventListener("click", function () {
+        document.getElementById("myModal").style.display = "block";
+    });
+
+    // Cerrar el modal al hacer clic en la "X" o en "Cerrar"
+    document.querySelector(".close").addEventListener("click", function () {
+        document.getElementById("myModal").style.display = "none";
+    });
